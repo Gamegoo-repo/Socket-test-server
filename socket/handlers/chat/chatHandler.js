21fbd1d4d2bde2a600d4755a1b8c79562104e03e
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-08 01:57:41 +0900

    채팅방 목록 조회 요청 및 room join시키기

--- socket/handlers/chat/chatHandler.js
+++ socket/handlers/chat/chatHandler.js
@@ -1,6 +1,15 @@
 const formatResponse = require("../../utils/common/responseFormatter");
+const { fetchChatroomUuid } = require("../../utils/chat/fetch");
 
 function handleChatEvents(socket, io) {
+  // 소켓 초기화 즉시, API서버로 채팅방 uuid 목록 조회 요청 실행
+  fetchChatroomUuid(socket).then((uuidList) => {
+    // uuidList는 ["05063951-6da7-4dbb-9134-3f8a7b6f8590", ...] 형태의 UUID 문자열 목록
+    uuidList.forEach((uuid) => {
+      socket.join(uuid);
+    });
+  });
+
   socket.on("chat message", (msg) => {
     io.emit("chat message", formatResponse(true, "success", { msg: msg }));
   });
--- socket/utils/common/responseFormatter.js
+++ socket/utils/common/responseFormatter.js
@@ -1,4 +1,4 @@
-// 응답을 포맷팅하는 함수
+// socket event 응답을 포맷팅하는 함수
 function formatResponse(status, message, data) {
   return {
     status: status,
