b382abdd51e199c0fcad32e6a9cbb1cfe7491fb2
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-11 03:26:41 +0900

    initializeFriend 메소드 수정

--- socket/handlers/friend/friendInitializer.js
+++ socket/handlers/friend/friendInitializer.js
@@ -2,17 +2,29 @@ const { fetchFriends } = require("../../utils/friend/fetch");
 const { emitFriendOnline, emitSetFriendList } = require("../../utils/friend/event");
 
 function initializeFriend(socket, io) {
-  let friendIdList = [];
-
   fetchFriends(socket) // 해당 회원의 친구 목록 조회 api 요청
-    .then((friends) => {
-      friendIdList = friends.map((friend) => friend.memberId);
+    .then(async (friends) => {
+      // 친구 중에서 현재 온라인인 친구의 소켓 id 및 memberId array 생성
+      const friendIdList = friends.map((friend) => friend.memberId);
+      let friendSocketList = [];
+      const connectedSockets = await io.fetchSockets();
+
+      console.log(`== member ID: ${socket.memberId}, friend's socket ID list START ==`);
+
+      for (const connSocket of connectedSockets) {
+        if (friendIdList.includes(connSocket.memberId)) {
+          console.log(`MemberId: ${connSocket.memberId}, SocketId: ${connSocket.id}`);
+          friendSocketList.push({ socketId: connSocket.id, memberId: connSocket.memberId });
+        }
+      }
+
+      console.log(`== member ID: ${socket.memberId}, friend's socket ID list END ==`);
 
       // 친구 소켓에게 "friend-online" event emit
-      emitFriendOnline(socket, io, friendIdList);
+      emitFriendOnline(io, friendSocketList);
 
       // 이 소켓에게 "set-friend-list" event emit
-      emitSetFriendList(socket, friendIdList);
+      emitSetFriendList(socket, friendSocketList);
     })
     .catch((error) => {
       console.error("Error fetching friends data:", error);
--- socket/utils/friend/event.js
+++ socket/utils/friend/event.js
@@ -2,34 +2,24 @@ const formatResponse = require("../common/responseFormatter");
 
 /**
  * 친구 소켓에게 내가 온라인 상태를 알리는 메소드
- * @param {*} socket
  * @param {*} io
- * @param {*} friendIdList
+ * @param {*} friendSocketList
  */
-function emitFriendOnline(socket, io, friendIdList) {
-  console.log(`== member ID: ${socket.memberId}, friend's socket ID list START ==`);
-  // 현재 연결된 모든 소켓 리스트 조회
-  const connectedSockets = io.sockets.sockets;
-
-  connectedSockets.forEach((connSocket, key) => {
-    // 연결된 모든 소켓 중, 친구의 소켓 찾기
-    if (friendIdList.includes(connSocket.memberId)) {
-      console.log(`MemberId: ${connSocket.memberId}, Key: ${key}`);
-
-      // 친구 소켓에게 friend-online emit
-      io.to(key).emit("friend-online", formatResponse(true, "friend-online", { memberId: socket.memberId }));
-    }
+function emitFriendOnline(io, friendSocketList) {
+  friendSocketList.forEach((friendSocket) => {
+    // 친구 소켓에게 friend-online emit
+    io.to(friendSocket.socketId).emit("friend-online", formatResponse(true, "friend-online", { memberId: friendSocket.memberId }));
   });
-  console.log(`== member ID: ${socket.memberId}, friend's socket ID list END ==`);
 }
 
 /**
- * 내 소켓에게 친구 목록을 초기화해야함을 알리는 메소드
+ * 내 소켓에게 온라인인 친구 목록을 초기화해야함을 알리는 메소드
  * @param {*} socket
- * @param {*} friendIdList
+ * @param {*} friendSocketList
  */
-function emitSetFriendList(socket, friendIdList) {
-  socket.emit("set-friend-list", formatResponse(true, "set-friend-list", { friendIdList }));
+function emitSetFriendList(socket, friendSocketList) {
+  const onlineFriendMemberIdList = friendSocketList.map((friend) => friend.memberId);
+  socket.emit("set-friend-list", formatResponse(true, "set-friend-list", { onlineFriendMemberIdList }));
 }
 
 module.exports = {
