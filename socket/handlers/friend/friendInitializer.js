cc553218c554da9eb7911e1ec50d9560ef27965b
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-13 00:21:28 +0900

    mapper 메소드 추가

--- socket/handlers/friend/friendInitializer.js
+++ socket/handlers/friend/friendInitializer.js
@@ -1,5 +1,6 @@
 const { fetchFriends } = require("../../utils/friend/fetch");
 const { emitFriendOnline, emitSetFriendList } = require("../../utils/friend/event");
+const { getSocketIdByMemberId } = require("../../utils/common/memberSocketIdMapper");
 
 /**
  * socket 초기화 즉시 실행될 메소드
@@ -11,19 +12,9 @@ function initializeFriend(socket, io) {
     .then(async (friends) => {
       // 친구 중에서 현재 온라인인 친구의 소켓 id 및 memberId array 생성
       const friendIdList = friends.map((friend) => friend.memberId);
-      let friendSocketList = [];
-      const connectedSockets = await io.fetchSockets();
 
-      console.log(`== member ID: ${socket.memberId}, friend's socket ID list START ==`);
-
-      for (const connSocket of connectedSockets) {
-        if (friendIdList.includes(connSocket.memberId)) {
-          console.log(`MemberId: ${connSocket.memberId}, SocketId: ${connSocket.id}`);
-          friendSocketList.push({ socketId: connSocket.id, memberId: connSocket.memberId });
-        }
-      }
-
-      console.log(`== member ID: ${socket.memberId}, friend's socket ID list END ==`);
+      // 친구 memberId로 socketId 찾기
+      const friendSocketList = await getSocketIdByMemberId(io, friendIdList);
 
       // 친구 소켓에게 "friend-online" event emit
       emitFriendOnline(io, friendSocketList, socket.memberId);
