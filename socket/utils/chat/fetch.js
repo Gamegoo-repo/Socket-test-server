dd81077f3aa5e62a6f1605f49a3cb7414ca58d02
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-13 02:40:39 +0900

    채팅 메시지 등록 api 요청 및 error 처리

--- socket/utils/chat/fetch.js
+++ socket/utils/chat/fetch.js
@@ -15,14 +15,38 @@ async function fetchChatroomUuid(socket) {
     if (response.data.isSuccess) {
       return response.data.result;
     } else {
-      throw new Error(`Failed to fetch chatroom uuid: ${response.data.message}`);
+      console.error("Failed to fetch chatroom uuid: ", response.data.message);
+      //throw new Error(`Failed to fetch chatroom uuid: ${response.data.message}`);
     }
   } catch (error) {
     console.error("Error fetching chatroom uuid data:", error);
+    //throw error;
+  }
+}
+
+async function postChatMessage(socket, chatroomUuid, message) {
+  try {
+    const response = await axios.post(
+      "http://localhost:8080/v1/chat/" + chatroomUuid,
+      { message: message },
+      {
+        headers: {
+          Authorization: `Bearer ${socket.token}`,
+        },
+      }
+    );
+    if (response.data.isSuccess) {
+      return response.data.result;
+    } else {
+      throw new Error(`Failed to post chat message: ${response.data.message}`);
+    }
+  } catch (error) {
+    console.error("Error fetching postChatMessage", error);
     throw error;
   }
 }
 
 module.exports = {
   fetchChatroomUuid,
+  postChatMessage,
 };
