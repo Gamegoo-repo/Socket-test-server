6139ba11fdc2195bada032a124506e732c551665
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-25 02:36:57 +0900

    my-message-broadcast-success event emit

--- socket/utils/chat/event.js
+++ socket/utils/chat/event.js
@@ -1,15 +1,19 @@
 const formatResponse = require("../common/responseFormatter");
 
 /**
- * uuid에 해당하는 room에 있는 socket 중 나를 제외한 socket에 broadcast
+ * chat message를 socket에 전달
  * @param {*} socket
  * @param {*} chatroomUuid
  * @param {*} data
  */
 function emitChatMessage(socket, chatroomUuid, data) {
-  //io.emit("chat-message", formatResponse("chat-message", { msg }));
   data.chatroomUuid = chatroomUuid;
+
+  // uuid에 해당하는 room에 있는 socket 중 나를 제외한 socket에 broadcast
   socket.broadcast.to("CHAT_" + chatroomUuid).emit("chat-message", formatResponse("chat-message", data));
+
+  // 내 socket에 message broadcast가 성공했음을 emit
+  socket.emit("my-message-broadcast-success", formatResponse("my-message-broadcast-success", data));
 }
 
 function emitUnreadCountUpdate(socket, chatroomUuid) {
